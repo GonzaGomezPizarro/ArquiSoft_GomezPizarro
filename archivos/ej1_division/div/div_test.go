633b3c62cf11a67917088fd33a7e0b4efeef02cf
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-04-05 17:23:08 -0300

    .

--- archivos/ej1_division/div/div_test.go
+++ archivos/ej1_division/div/div_test.go
@@ -1,22 +1,62 @@
 package div
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestCase struct {
+	a              float32
+	b              float32
+	ExpectedResult float32
+	ExpectedError  bool
+}
 
 func TestDivisionOK(t *testing.T) { //libreria testing (https://golang.org/pkg/testing/)
-	Division, err := Division(10, 2)
-	if err != nil {
-		t.Error(err)
-		return
+	Divisio, err := Division(10, 2)
+
+	// if err != nil {
+	// 	t.Error(err)
+	// 	return
+	// }
+	assert.Nil(t, err) //libreria testing (https://golang.org/pkg/testing/)
+
+	// if Division != 5 {
+	// 	t.Error("Resultado incorrecto")
+	// 	return
+	// }
+	assert.Equal(t, Divisio, float32(5)) // es lo mismo que el anterior
+
+	for i := -2; i < 10; i++ {
+		Divisio, err := Division(float32(i), 1.77)
+		assert.Nil(t, err)
+		assert.Equal(t, Divisio, float32(i)/1.77)
 	}
-	if Division != 5 {
-		t.Error("Resultado incorrecto")
-		return
+
+	//Otra Forma:
+	testcases := []TestCase{
+		{a: 10, b: 2, ExpectedResult: 5, ExpectedError: false},
+		{a: 10, b: 0, ExpectedResult: 999999, ExpectedError: true},
+		{a: -10, b: 2, ExpectedResult: -5, ExpectedError: false},
+		{a: -10, b: 0, ExpectedResult: 999999, ExpectedError: false}, // este esta mal para ver como muestra el error
+		{a: 10, b: -2, ExpectedResult: -5, ExpectedError: false},
+		{a: .5, b: 2, ExpectedResult: .24, ExpectedError: false}, // este esta mal para ver como muestra el error
 	}
+	for _, testcase := range testcases {
+		Divisio, err := Division(testcase.a, testcase.b)
+		assert.Equal(t, testcase.ExpectedResult, Divisio)
+		assert.Equal(t, testcase.ExpectedError, err != nil)
+	}
+
 }
 func TestDivisionErrorbyZero(t *testing.T) { //libreria testing (https://golang.org/pkg/testing/)
 	_, err := Division(10, 0)
-	if err == nil {
-		t.Error("Error no esperado")
-		return
-	}
+
+	// if err == nil {
+	// 	t.Error("Error no esperado")
+	// 	return
+	// }
+	assert.NotNil(t, err) //libreria testing (https://golang.org/pkg/testing/)
+
 }
--- archivos/ej1_division/sum/sum_test.go
+++ archivos/ej1_division/sum/sum_test.go
@@ -1,7 +1,16 @@
 package sum
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestSum(t *testing.T) {
-	Sum(2, 4)
+	for i := -8; i < 12; i++ {
+		for j := 12; j > -8; j-- {
+			assert.Equal(t, float64(i+j), Sum(float64(i), float64(j)))
+		}
+	}
+
 }
