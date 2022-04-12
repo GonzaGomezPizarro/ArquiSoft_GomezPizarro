49acaa815ccbc00b6e0ead20eb766161f5584db6
GonzaGomezPizarro <1912988@ucc.edu.ar>
2022-04-12 18:31:56 -0300

    Repositorio atualizado

--- archivos/API/main.go
+++ archivos/API/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin" // tira error
+	"github.com/gin-gogo gnic/gin" // tira error
 )
 
 func main() {
@@ -9,4 +9,18 @@ func main() {
 	engine.GET("/ping", Ping)
 	engine.Run(":3000")
 
+}
+
+func Test (ctx *gin.Context) {
+	var body body
+	err:= ctx.BindJSON(&body)
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, body)
+}
+
+func Ping (ctx *gin.Context) {
+	ctx.String(200, "pong")
 }
